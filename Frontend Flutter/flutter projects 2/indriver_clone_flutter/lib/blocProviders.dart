96cd55a79bb2524c5316f0e288774a205d863d1f
Malamia09 <juanbaezagit@gmail.com>
2026-02-25 11:15:28 -0600

    Add files via upload

---
+++ Frontend Flutter/flutter projects 2/indriver_clone_flutter/lib/blocProviders.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/login/bloc/LoginBloc.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/login/bloc/LoginEvent.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/register/bloc/RegisterBloc.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/register/bloc/RegisterEvent.dart';
+
+List<BlocProvider> blocProviders = [
+  BlocProvider<LoginBloc>(
+    create: (context) => LoginBloc()..add(LoginInitEvent()),
+  ),
+  BlocProvider<RegisterBloc>(
+    create: (context) => RegisterBloc()..add(RegisterInitEvent()),
+  ),
+];
---
+++ Frontend Flutter/flutter projects 2/indriver_clone_flutter/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:indriver_clone_flutter/blocProviders.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/login/LoginPage.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/login/bloc/LoginBloc.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/login/bloc/LoginEvent.dart';
+import 'package:indriver_clone_flutter/src/presentation/pages/authentication/register/RegisterPage.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: blocProviders,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: 'register',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'register': (BuildContext context) => RegisterPage(),
+        },
+      ),
+    );
+  }
+}
